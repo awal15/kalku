514dbd7a4a13360a7d8cf79d762181c0df594473
awal15 <59858826+awal15@users.noreply.github.com>
2020-04-05 12:07:12 +0700

    Update kalku.cpp

--- kalku.cpp
+++ kalku.cpp
@@ -3,49 +3,56 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
   float a,b;
-  int kode;
+  int code;
   
   menu :
-    cout << "Kalkulator Sederhana\n";
-    cout << "Author : Awalramadhan\n\n";
-    cout << "Silahkan Dipilih Gan <__>\n\n";
-    cout << "Pilihan Menu Nya Gan : \n";
-    cout << "1. penjumlahan (+)\n";
-    cout << "2. pengurangan (-)\n";
-    cout << "3. perkalian (*)\n";
-    cout << "4. pembagian (/)\n";
-    cout << "Silahkan masukkan kode menu : ";cin >> kode;
+    cout <<"Simple Calculator\n";
+    cout <<"Author : from Indonesian\n";
+  test :
+    cout <<"Menu Options : \n";
+    cout <<"1.Addition\n";
+    cout <<"2.Subtraction\n";
+    cout <<"3.Multiplication\n";
+    cout <<"4.Division\n";
+    cout <<"\nPlease Enter The Menu Code : ";cin >> code;
     
-    if (kode > 4 || kode < 1){
-      cout << "kode salah \n silahkan ulangi kode .";
-      system ("pause");
-      system ("cls");
+    if (code > 4 || code < 1 )
+    {
+      cout <<"Incorrect Code Please Repeat Again\n\n";
+      system ("break");
       goto menu;
     }
-    else {
-      cout << "\nMasukkan Angka Pertama : ";cin >>a;
-      cout << "\nMasukkka Angka Kedua : ";cin >>b;
-      if (kode == 1){
-        cout << "\nHasil Penjumlahan dari "<<a<<" + "<<b<<" adalah : "<<a+b<<endl;
-      }
-      else if (kode == 2){
-        cout << "\nHasil Pengurangan dari "<<a<<" - "<<b<<" adalah : "<<a-b<<endl;
-      }
-      else if (kode == 3){
-        cout << "\nHasil Perkalian dari "<<a<<" * "<<b<<" adalah : "<<a*b<<endl;
-      }
-      else if (kode == 4){
-        cout << "\nHasil Pembagian dari "<<a<<" / "<<b<<" adalah : "<<a/b<<endl;
-      }
+    else
+    {
+      cout <<"\nEnter The First Number : ";cin >> a;
+      cout <<"\nEnter The Second Number : ";cin >> b;
+    if (code == 1)
+    {
+      cout <<"\nAddtions Result "<<a<< " + "<<b<< " is : "<<a+b<<endl;
+      system ("break");
+      goto test;
     }
+    else if (code == 2)
+    {
+      cout <<"\nSubtraction Result "<<a<< " - "<<b<< " is : "<<a-b<<endl;
+      system ("break");
+      goto test;
+    }
+    else if (code == 3)
+    {
+      cout <<"\nMultiplication Result "<<a<< " * "<<b<< " is : "<<a*b<<endl;
+      system("break");
+      goto test;
+    }
+    else if (code == 4)
+    {
+      cout <<"\nDivision Result "<<a<< " / "<<b<< " is : "<<a/b<<endl;
+      system ("break");
+      goto test;
+    }
+    }
+    return 0;
 }
-
-
-
-
-
-
-
-
